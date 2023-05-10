a780aebcd68238ada284acdb1f2f3faa6aefdc5a
Mayerling Joseph <josephmayerling@gmail.com>
2023-05-10 14:20:21 -0400

    word-memory

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
 const boardContainer = document.querySelector('.board-container')
-let cards = []
+let cards = [
+  { name: 'B1', img: 'https://i.imgur.com/p8ejsXM.png' },
+  { name: 'B2', img: 'https://i.imgur.com/efgP9Mm.png' },
+  { name: 'B3', img: 'https://i.imgur.com/Hp4b4OW.png' },
+  { name: 'B4', img: 'https://i.imgur.com/vqAvlip.png' },
+  { name: 'B5', img: 'https://i.imgur.com/O9LBr5L.png' },
+  { name: 'B6', img: 'https://i.imgur.com/ZNLQKqF.png' }
+]
 let firstCard, secondCard
 let lockBoard = false
 let score = 0
@@ -26,11 +33,35 @@ function generateCards() {
     cardElement.setAttribute('data-name', card.name)
     cardElement.innerHTML = `
       <div class="front">
-        <img class="front-image" src=${card.image} />
+        <img class="front-image" src="${card.image}" />
       </div>
       <div class="back"></div>
     `
-    gridContainer.appendChild(cardElement)
+    boardContainer.appendChild(cardElement)
     cardElement.addEventListener('click', flipCard)
   }
+}
+function flipCard() {
+  if (lockBoard) return
+  if (this === firstCard) return
+
+  this.classList.add('flipped')
+
+  if (!firstCard) {
+    firstCard = this
+    return
+  }
+
+  secondCard = this
+  score++
+  document.querySelector('.score').textContent = score
+  lockBoard = true
+
+  checkForMatch()
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name
+
+  isMatch ? disableCards() : unflipCards()
 }
